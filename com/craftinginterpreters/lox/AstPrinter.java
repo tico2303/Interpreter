fdae26745608a04eba2652430d9dcfe528e5b918
Robert Martinez <tico82003@gmail.com>
2021-08-08 20:28:11 -0700

    adding Parser

--- com/craftinginterpreters/lox/AstPrinter.java
+++ com/craftinginterpreters/lox/AstPrinter.java
@@ -37,6 +37,7 @@ public class AstPrinter implements Expr.Visitor<String> {
     }
 
     public static void main(String[] args){
+        /*
         Expr expression = new Expr.Binary(
             new Expr.Unary(
                 new Token(TokenType.MINUS, "-",null,1), 
@@ -44,6 +45,12 @@ public class AstPrinter implements Expr.Visitor<String> {
              new Token(TokenType.STAR, "*",null,1), 
              new Expr.Grouping(
                  new Expr.Literal(45.67)));
+        */
+        Expr expression = new Expr.Binary(
+                new Expr.Binary(new Expr.Literal(1), new Token(TokenType.PLUS, "+", null,1), new Expr.Literal(2)), 
+                new Token(TokenType.STAR, "*",null,1), 
+                new Expr.Binary(new Expr.Literal(4), new Token(TokenType.MINUS, "-", null, 1), new Expr.Literal(3))
+                );
         System.out.println(new AstPrinter().print(expression));
     }
 }
