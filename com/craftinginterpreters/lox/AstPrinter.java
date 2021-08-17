f5a97bed2207aad3521265a05b30a3f19704b89f
Robert Martinez <tico82003@gmail.com>
2021-08-17 10:36:30 -0700

    working variable declarations

--- com/craftinginterpreters/lox/AstPrinter.java
+++ com/craftinginterpreters/lox/AstPrinter.java
@@ -1,6 +1,6 @@
 package com.craftinginterpreters.lox;
 
-
+/*
 public class AstPrinter implements Expr.Visitor<String> {
     String print(Expr expr){
         return expr.accept(this);
@@ -37,15 +37,6 @@ public class AstPrinter implements Expr.Visitor<String> {
     }
 
     public static void main(String[] args){
-        /*
-        Expr expression = new Expr.Binary(
-            new Expr.Unary(
-                new Token(TokenType.MINUS, "-",null,1), 
-                new Expr.Literal(123)),
-             new Token(TokenType.STAR, "*",null,1), 
-             new Expr.Grouping(
-                 new Expr.Literal(45.67)));
-        */
         Expr expression = new Expr.Binary(
                 new Expr.Binary(new Expr.Literal(1), new Token(TokenType.PLUS, "+", null,1), new Expr.Literal(2)), 
                 new Token(TokenType.STAR, "*",null,1), 
@@ -54,3 +45,4 @@ public class AstPrinter implements Expr.Visitor<String> {
         System.out.println(new AstPrinter().print(expression));
     }
 }
+*/
