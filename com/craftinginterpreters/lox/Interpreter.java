4da455c1d4e5453f3a3b9b010d858afe9ca408a1
Robert Martinez <tico82003@gmail.com>
2021-08-26 11:36:13 -0700

    scope resolution added

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -1,6 +1,8 @@
 package com.craftinginterpreters.lox;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 class Interpreter implements Expr.Visitor<Object>,
                              Stmt.Visitor<Void>{
@@ -9,6 +11,7 @@ class Interpreter implements Expr.Visitor<Object>,
     final Environment globals = new Environment();
     //enviorment changes as we enter/exit local scopes
     private Environment environment = globals;
+    private final Map<Expr, Integer> locals = new HashMap<>();
 
     Interpreter(){
         //adding native/built-in function 'clock' that implements LoxCallable
@@ -64,7 +67,15 @@ class Interpreter implements Expr.Visitor<Object>,
     }
     @Override
     public Object visitVariableExpr(Expr.Variable expr){
-        return environment.get(expr.name);
+        return lookUpVariable(expr.name, expr);
+    }
+    private Object lookUpVariable(Token name, Expr expr){
+        Integer distance = locals.get(expr);
+        if (distance != null){
+            return environment.getAt(distance, name.lexeme);
+        } else{
+            return globals.get(name);
+        }
     }
     private void checkNumberOperand(Token operator, Object operand){
         if (operand instanceof Double) return;
@@ -177,6 +188,9 @@ class Interpreter implements Expr.Visitor<Object>,
     private void execute(Stmt stmt){
         stmt.accept(this);
     }
+    void resolve(Expr expr, int depth){
+        locals.put(expr, depth);
+    }
     void executeBlock(List<Stmt> statements,Environment environment){
         Environment previous = this.environment;
         try{
@@ -248,7 +262,13 @@ class Interpreter implements Expr.Visitor<Object>,
     @Override 
     public Object visitAssignExpr(Expr.Assign expr){
         Object value = evaluate(expr.value);
-        environment.assign(expr.name, value);
+
+        Integer distance = locals.get(expr);
+        if (distance != null){
+            environment.assignAt(distance, expr.name, value);
+        }else{
+            globals.assign(expr.name, value);
+        }
         return value;
     }
     private void checkNumberOperands(Token operator, Object left, Object right){
