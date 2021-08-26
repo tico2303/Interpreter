a2ebff2c9ae0e3f4d75615004b16c6fc38d4f8fc
Robert Martinez <tico82003@gmail.com>
2021-08-26 11:37:28 -0700

    scope resolution added

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -53,7 +53,12 @@ public class Lox{
 
         //stop if there was a syntax error.
         if(hadError) return;
-        //System.out.println(new AstPrinter().print(expression));
+
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+        //Stop if there was a resolution error
+        if(hadError) return;
+
         interpreter.interpret(statements);
 
     }
